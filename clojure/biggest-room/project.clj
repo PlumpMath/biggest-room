cf20db0a75f90e8f1f02d075d5f13ce241041849
Nick (Yedikule) <nick@cassiel.com>
2016-01-18 16:19:29 +0000

    Properties.

--- clojure/biggest-room/project.clj
+++ clojure/biggest-room/project.clj
@@ -7,6 +7,7 @@
                  [clj-http "2.0.0"]
                  [com.stuartsierra/component "0.3.1"]
                  [twitter-api "0.7.8"]
+                 [clojurewerkz/propertied "1.2.0"]
                  [org.clojure/data.json "0.2.6"]]
   :plugins [[cider/cider-nrepl "0.9.1"]
             [refactor-nrepl "1.1.0"]
--- clojure/biggest-room/scratch.clj
+++ clojure/biggest-room/scratch.clj
@@ -1,3 +1,14 @@
-(ns user)
+(ns user
+  (:require [clojurewerkz.propertied.properties :as p]
+            [clojure.java.io :as io]))
+
+;; ---
+
+(-> (p/load-from (io/resource "test.properties"))
+    (p/properties->map true))
+
+;; ---
 
 (reset)
+
+(stop)
