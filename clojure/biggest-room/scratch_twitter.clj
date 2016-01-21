1f48bd7af7024836138d423de77bc4b8bd08a719
Nick (Yedikule) <nick@cassiel.com>
2016-01-21 15:47:34 +0000

    Stream working.

--- clojure/biggest-room/scratch_twitter.clj
+++ clojure/biggest-room/scratch_twitter.clj
@@ -18,7 +18,7 @@
 
 
 
-(:config (:config S))
+(:entries (:config S))
 
 
 (def twitter (get-in S [:config :entries :twitter]))
@@ -32,13 +32,33 @@ user/my-creds
 
 (users-show :oauth-creds my-creds :params {:screen-name "cassieldotcom"})
 
+(def A (atom nil))
+
 ; retrieves the user stream, waits 1 minute and then cancels the async call
-(def ^:dynamic *response* (user-stream :oauth-creds my-creds))
+(def ^:dynamic *response* (user-stream
+                           :oauth-creds my-creds
+                           :callbacks (AsyncStreamingCallback. #_ (comp println #(:text %) json/read-json #(str %2))
+                                                               (fn [& x] (reset! A x))
+                                                               (comp println response-return-everything)
+                                                               exception-print)))
 
+(nth @A 0)
+
+(-> (nth @A 1)
+    str
+    json/read-json
+    )
+
+*response*
 (meta *response*)
-
-(Thread/sleep 60000)
 ((:cancel (meta *response*)))
+
+(:body (friendships-show :oauth-creds my-creds
+                        :params {:target-screen-name "cassieldotcom"}))
+
+
+((fn [& x] x) 1 2)
+
 
 ; supply a callback that only prints the text of the status
 (def ^:dynamic
@@ -47,8 +67,18 @@ user/my-creds
                               (comp println response-return-everything)
                               exception-print))
 
-(statuses-filter :params {:track "Bowie"}
-                 :oauth-creds my-creds
-                 :callbacks *custom-streaming-callback*)
+(def X (statuses-filter :params {:track "hello"}
+                        :oauth-creds my-creds
+                        :callbacks *custom-streaming-callback*))
+
+(meta X)
+((:cancel (meta X)))
+
+(def X (user-stream :oauth-creds my-creds
+                    :callbacks *custom-streaming-callback*))
+
+(:body X)
 
 *custom-streaming-callback*
+
+(println "A")
